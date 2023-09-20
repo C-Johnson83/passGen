693885294121a03ac7d923bffed89bda3a766964
chris <chrisj21283@gmail.com>
2023-09-20 19:31:54 -0400

    added prompts and generator function

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,32 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var password = "";
+  var chars
+  var count = prompt("Between 8 and 128 characters,\n\n how many characters would you like your password to be?")
+
+  if (count < 8) {
+    alert("The password must be a minimum of 8 characters.\n\nPlease try again");
+    generatePassword();
+  } else if (count > 128) {
+    alert("Calm down a little bit, keep it under 128 characters please.\n\nTry again");
+    generatePassword();
+  }
+
+  var symbols = prompt(" would you like to use special characters?\n\nAnswer with y or n")
+
+  if (symbols !== "Y") {
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  } else {
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'" + '!"#$%&()*+,-./:;<=?@[]^_`{|}~';
+  }
+  for (var c = 0; (c <= count); c++) {
+    var rand = Math.floor(Math.random() * chars.length);
+    password += chars.charAt(rand)
+  }
+  console.log(password);
+  return password;
+}
+
